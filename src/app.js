2a484f99c332c68058eab59765dd24318b7aca11
Piyush Patil <piyushpatil1741@gmail.com>
2025-11-08 13:35:03 +0530

    Explored GET,POST,PATCH,DELETE,PUT

--- src/app.js
+++ src/app.js
@@ -2,11 +2,34 @@ const express = require('express');
 
 const app = express();
 
+
+
 app.use("/test",(req,res)=>{
     res.send("Hello I am Piyush Patil")
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello Hello Hello Hello")
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Piyush",
+        lastName: "Patil"
+    })
+})
+
+app.post("/user",(req,res)=>{
+    console.log("Save Data to the database");
+    res.send("Data Succesfully saved to database.");
+})
+
+app.delete("/user",(req,res)=>{
+    console.log("Data is being Deleted");
+    res.send("Deleted.")
+})
+
+app.put("/user",(req,res)=>{
+    console.log("User name changed.");
+    res.send("User Updated")
+})
+app.patch("/user",(req,res)=>{
+    console.log("User name patched.");
+    res.send("User Patched")
 })
 
 app.listen(3000,()=>{
